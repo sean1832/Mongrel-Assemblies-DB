cb71e881e2a9b331ff584ef734cd6ddfd78f168c
Zeke Zhang <84948038+sean1832@users.noreply.github.com>
2023-08-24 14:12:15 +1000

    renamed from storage_handler.py

--- src/sftp_op.py
+++ src/sftp_op.py
@@ -1,3 +1,4 @@
+import os
 import streamlit as st
 import paramiko
 from paramiko import RSAKey
@@ -13,8 +14,9 @@ key_data = st.secrets["sftp"]["key_data"]
 
 # --------------------------------------------
 
+# SFTP functions
 @contextmanager
-def create_ssh_client(ip, port, username, password):
+def create_ssh_client():
     client = paramiko.SSHClient()
     ssh_host_key = RSAKey(data=base64.b64decode(key_data.encode()))
     client.get_host_keys().add(ip, 'ssh-rsa', ssh_host_key)
@@ -34,10 +36,26 @@ def create_sftp_client(ssh_client):
         sftp_client.close()
 
 
+def create_remote_dir(sftp_client, remote_dir):
+    """Recursively create drectories on the remote server"""
+    dirs_to_create = []
+    while True:
+        try:
+            sftp_client.stat(remote_dir)
+            break
+        except FileNotFoundError:
+            dirs_to_create.append(remote_dir)
+            remote_dir = os.path.dirname(remote_dir)
+    for dir_to_create in reversed(dirs_to_create):
+        sftp_client.mkdir(dir_to_create, mode=0o755)
+
+
 def upload_file(filename, local_dir, remote_dir):
-    """Upload file from local machine to remote server"""
-    with create_ssh_client(ip, port, username, password) as ssh_client:
+    with create_ssh_client() as ssh_client:
         with create_sftp_client(ssh_client) as sftp_client:
+            # Check if remote directory exists, if not create it
+            create_remote_dir(sftp_client, remote_dir)
+
             remote_full_path = f"{remote_dir}/{filename}"
             local_full_path = f"{local_dir}/{filename}"
             sftp_client.put(local_full_path, remote_full_path)
@@ -45,9 +63,8 @@ def upload_file(filename, local_dir, remote_dir):
 
 def download_file(filename, remote_dir, local_dir):
     """Download file from remote server to local machine"""
-    with create_ssh_client(ip, port, username, password) as ssh_client:
+    with create_ssh_client() as ssh_client:
         with create_sftp_client(ssh_client) as sftp_client:
             remote_full_path = f"{remote_dir}/{filename}"
             local_full_path = f"{local_dir}/{filename}"
             sftp_client.get(remote_full_path, local_full_path)
-
